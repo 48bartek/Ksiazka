25d368826d8439f75a9ab26578c6dbab63f98fe2
48bartek <4884bartek@gmail.com>
2018-01-22 17:47:50 +0100

    sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219103457) do
+ActiveRecord::Schema.define(version: 20180115205354) do
 
   create_table "directions", force: :cascade do |t|
     t.text "step"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 20171219103457) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
